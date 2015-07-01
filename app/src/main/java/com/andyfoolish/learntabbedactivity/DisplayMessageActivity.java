ab7c3097d663abc8cc100ae4c323d3d0a8e0376e
Liu Chao <foolishandy@gmail.com>
2015-07-01 15:38:53 +0800

    add display_message_activity

---
+++ app/src/main/java/com/andyfoolish/learntabbedactivity/DisplayMessageActivity.java
@@ -0,0 +1,31 @@
+package com.andyfoolish.learntabbedactivity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/andyfoolish/learntabbedactivity/MainActivity.java
+++ app/src/main/java/com/andyfoolish/learntabbedactivity/MainActivity.java
@@ -1,19 +1,33 @@
 package com.andyfoolish.learntabbedactivity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    //作为putExtra()的第一个参数--键名
+    private final static String EXTRA_MESSAGE="com.dlut.learntabbedactivity.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, TabbedActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        //把EditText的文本内容关联到一个本地 message 变量
+        String message = editText.getText().toString();
+        //使用putExtra()方法把值传给intent
+        intent.putExtra(EXTRA_MESSAGE, message);
+        //运行这个方法，系统收到我们的请求后会实例化在Intent中指定的Activity
+        startActivity(intent);
+    }
 
 
 }
